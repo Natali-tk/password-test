1149e6d9228a980855028084896a29356a7658ec
Natali-tk <natasha.totska@yahoo.com>
2024-11-26 12:39:56 +0200

    Changed font size in input, cleaned code

--- src/assets/components/Input/Input.tsx
+++ src/assets/components/Input/Input.tsx
@@ -12,40 +12,11 @@ export const Input=({
   ...props
 }: StadartProps) => {
   const [show, setShow] = useState(false)
-  // const { status, setStatus } = useContext(PasswordContext)
-  // const strongRegex = /^([a-z]+\d+\W+)$/i 
-  
-  // const enoughRegex = /^([a-z]+|\d+|\W+)$/i
-  // const letDig = /^([a-z]+\d+)$/i   
-  // const letSym = /^([a-z]+\W+)$/i
-  // const digSym = /^(\d+\W+)$/i
-  
  
   const handleClick = () => {
     setShow(!show)
   }
 
-  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const newValue = e.target.value;
-  //   console.log(newValue);
-  //   if (newValue.trim() === "") {
-  //     setStatus(EPasswordStatus.empty)
-  //   } else if (newValue.length < 8) {
-  //     setStatus(EPasswordStatus.small)
-  //   }
-  //     else if (newValue.match(enoughRegex)) {
-  //     setStatus(EPasswordStatus.easy)
-  //   }
-  //   else if (newValue.match(letSym) || newValue.match(letDig) || newValue.match(digSym)) {
-  //     setStatus(EPasswordStatus.medium)
-  //   }
-  //   else if (newValue.match(strongRegex)) {
-  //     setStatus(EPasswordStatus.strong)
-  //   } else {
-  //     setStatus(EPasswordStatus.empty)
-  //   }
-  // }
-  // console.log(status);
   return(
     <div className={s.form__input}>
       <label htmlFor={id} className={s.label}>{label}<span> *</span></label>
